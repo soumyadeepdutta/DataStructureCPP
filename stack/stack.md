604a67131b46caafd0443b43110b640e6dc833eb
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2020-12-30 20:24:23 +0530

    Stack array implementation

---
+++ stack/stack.md
@@ -0,0 +1,18 @@
+# Stack
+Stack is a linear data structure.
+Data operations are performed in LIFO order. Data insertion take place in one end, deletion in the another. It use a property 'top' to access data in differnt location. By default top = -1 when stack is empty.
+
+## Property
+There are three main properties of Stack
+1. Push -> Data insertion at top
+2. Pop -> Return and remove data from top
+3. isEmpty -> Returns true if top is -1, else false
+
+Other properties
+
+4. Print -> Print stack data
+5. Peek -> Print top data (not removing)
+6. Size -> Returns maximum capacity
+
+## Implementation
+Stack can be implemente in two ways. Using array or linked list.
---
+++ stack/stack_array.cpp
@@ -0,0 +1,86 @@
+/*
+Stack is a Linear data structure 
+order: LIFO
+properties: top, isEmpty
+operaations: push, pop 
+*/
+
+#include <iostream>
+using namespace std;
+
+// maximum capacity of stack
+#define MAX 10
+
+// stcak array implementation
+class Stack
+{
+    // private
+    int top;
+    int arr[MAX];
+
+public:
+    Stack()
+    {
+        top = -1;
+    }
+    bool isEmpty();
+    void push(int x);
+    int pop();
+    void print();
+};
+
+bool Stack::isEmpty()
+{
+    return (top < 0);
+}
+
+void Stack::print()
+{
+    if (isEmpty())
+    {
+        cout << "Empty stack \n";
+    }
+    else
+    {
+        for (int i = 0; i <= top; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+}
+
+void Stack::push(int x)
+{
+    if (top >= MAX - 1)
+        cout << "Overflow";
+    else
+    {
+        arr[++top] = x;
+        cout << "Pushed " << x << endl;
+    }
+}
+
+int Stack::pop()
+{
+    if (isEmpty())
+    {
+        cout << "Underflow \n";
+        return 0;
+    }
+    else
+    {
+        int x = arr[top--];
+        return x;
+    }
+}
+
+int main(void)
+{
+    Stack s;
+    cout << s.isEmpty() << endl;
+    s.print();
+    s.pop();
+    s.push(5);
+    cout << s.pop() << endl;
+    s.print();
+    return 0;
+}
